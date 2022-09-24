fa377cdc9bc47eb71f09e394a70e9b5cc0a92939
justingit <perl@justinsimoni.com>
2022-09-24 01:17:04 -0600

    my head hurts.

--- core5_filemanager/connectors/pl/filemanager.pl
+++ core5_filemanager/connectors/pl/filemanager.pl
@@ -3,7 +3,9 @@
 # So remember we need to copy this into dada/installer-bak...
 #(how should we do that?!)
 
-#use CGI::Carp qw(fatalsToBrowser);
+use CGI::Carp qw(fatalsToBrowser);
+
+use Carp qw(croak carp);
 
 use FindBin;
 
@@ -19,13 +21,17 @@ use lib "$FindBin::Bin/../../../../cgi-bin/dada";
 use lib "$FindBin::Bin/../../../../cgi-bin/dada/DADA/perllib";
 use lib "$FindBin::Bin/../../../../cgi-bin/dada/DADA/App/Support";
 
-# <!-- tmpl_if additional_perllibs -->
+# 
 # Additional Perl Libraries:
-#<!-- tmpl_loop additional_perllibs -->
-		use lib "<!-- tmpl_var name default='' -->";
-# <!-- /tmpl_loop -->
+#
+		use lib "/Users/justin/testserver/dada";
+# 
+		use lib "/Users/justin/testserver/dada/DADA/perllib";
+# 
+		use lib "/Users/justin/testserver/dada/DADA/App/Support";
+# 
 #/ Additional Perl Libraries:
-#<!-- /tmpl_if -->
+#
 
 #die $FindBin::Bin;
 
@@ -96,6 +102,19 @@ my $MODE_MAPPING = {
 sub main {
     $q = CGI->new;
     my $method = $MODE_MAPPING->{ scalar $q->param('mode') } || \&root;
+	
+	# This is a kludge: 
+	my $p = $q->param('path');
+	warn '$p: ' . $p if $t; 
+	# A url, with 
+	#my $up = quotemeta($config->{url_path}); 
+	# This is a thing that is happening, 
+	if($p =~ m/^\/http\:\//){ 
+		$p =~ s/^\/http\:\//http:\/\//;
+		warn 'new one: ' . $p if $t;
+		 $q->param('path', $p);
+	}
+	
 
     unless ( $q->param('mode') eq "download" || $q->param('mode') eq "add" ) {
         print $q->header('application/json');
@@ -106,6 +125,9 @@ sub main {
 #?mode=getinfo&path=/UserFiles/Image/logo.png&getsize=true
 #For now return image size info anyway
 sub getinfo {
+	
+	warn "\nin getinfo\n" if $t; 
+	
     return unless params_valid( [qw(path)] );
 
     my $filename = relative_file_name_from_url( scalar $q->param('path') );
@@ -114,9 +136,13 @@ sub getinfo {
 }
 
 sub file_info {
+	
+	warn 'in file_info' if $t;
+	
     my $rel_filename = shift;
-    my $abs_filename = absolute_file_name_from_url($rel_filename);
-    my $url_filename = url_for_relative_filename($rel_filename);
+    my $abs_filename = absolute_file_name_from_relative($rel_filename);
+    my $url_filename = url_from_relative_filename($rel_filename);
+	
 
     my $info = image_info($abs_filename);
     my ( $fileparse_filename, $fileparse_dirs, $fileparse_suffix ) =
@@ -140,6 +166,11 @@ sub file_info {
     elsif ( -e '../../' . $config_js->{icons}{path} . $suffix . '.png' ) {
         $preview = $config_js->{icons}{path} . $suffix . '.png';
     }
+	
+	
+	#my $return_path = 
+	
+
     return {
         "Path"       => $url_filename,
         "Filename"   => $fileparse_filename,
@@ -157,55 +188,132 @@ sub file_info {
     };
 }
 
+sub test_rel { 
+	my $fn = shift;
+	my $upload_rel = $config_js->{options}->{fileRoot}; 
+  	   $upload_rel =~ s/\/$//g; 
+  	   $upload_rel = quotemeta($upload_rel);
+	if($fn =~ !/^$upload_rel/){ 
+		croak "relative wrong: " . $fn; 
+	}	
+}
+
+sub test_abs { 
+	my $fn = shift; 
+	my $upload_abs = $config->{uploads_directory};
+	   $upload_abs =~ s/\/$//g; 
+	   $upload_abs = quotemeta($upload_abs);
+	 
+	if($fn !~ m/^$upload_abs/){ 
+		croak "abs wrong: " . $fn; 
+	}	
+}
+
+sub test_url { 
+	
+	my $url = shift; 
+	my $upload_url = $config->{url_path};
+	   $upload_url =~ s/\/$//g; 
+	   $upload_url = quotemeta($upload_url);
+	 
+	if($url !~ m/^$upload_url/){ 
+		croak "url wrong: " . $url; 
+	}	
+	
+}
+
 # ?mode=getfolder&path=/UserFiles/Image/&getsizes=true&type=images
 #Ignoring type for now
+
 sub getfolder {
-    warn 'in getfolder'
+    warn "\nin getfolder\n"
       if $t;
 
     return unless params_valid( [qw(path)] );
 
     my @directory_list = ();
 
-    warn '$q->param(\'path\'): ' . $q->param('path') if $t;
+	
 
-    # So path sometimes has an abs path and sometimes a URL. OK.
+    warn '$q->param(\'path\'): ' . $q->param('path') 
+		if $t;
+
     my $rel_directory;
-    if ( $q->param('path') =~ m/^\// ) {
-        $rel_directory =
-          relative_file_name_from_absolute( scalar $q->param('path') );
+	
+	my $fr = $config_js->{options}->{fileRoot};; 
+	   $fr =~ s/\/$//;
+	   $fr = quotemeta($fr);
+	
+    if ( $q->param('path') =~ m/^$fr/ ) {
+		# is this already a relative dir? 
     }
-    else {
+    elsif($q->param('path') =~ m/^http/ ) {
         $rel_directory =
           relative_file_name_from_url( scalar $q->param('path') );
     }
+	else { 
+		# fake it, 
+		$rel_directory = rel_fn_from_fn($rel_directory); 
+		warn 'faking it: ' if $t; 
+	}
 
     warn '$rel_directory: ' . $rel_directory
       if $t;
 
-    my $directory = absolute_file_name_from_relative($rel_directory);
+	 test_rel($rel_directory);
 
-    warn '$directory; ' . $directory
-      if $t;
+
+    my $directory = absolute_file_name_from_relative($rel_directory);
+	test_abs($directory);
+
 
     my @directories = File::Find::Rule->maxdepth(1)->directory->in($directory);
     my @files       = File::Find::Rule->maxdepth(1)->file->in($directory);
 
+
+
     foreach my $dir (@directories) {
-        my $url_filename =
-          url_for_relative_filename( relative_file_name_from_absolute($dir) );
-
+		
+			
+		my $just_dn = fn_from_rel_fn(relative_file_name_from_absolute($dir)); 
+		warn '$just_dn; ' . $just_dn if $t; 
+		# skip dot files/dirs
+		if($just_dn =~ m/^\./){ 
+			next; 
+		}
+				
+        my $url_filename = url_from_absolute_filename($dir); 
+		
+		#die '$url_filename: ' . $url_filename; 
+		
         #Skip current directory
-        next if relative_file_name_from_absolute($dir) eq $rel_directory;
+		#warn 'looking at dir: ' . $dir; 
+		#warn 'SAME1: ' . relative_file_name_from_absolute($dir); 
+		#warn 'SAME2: ' . $rel_directory; 
+		
+        if(
+			fn_from_rel_fn(relative_file_name_from_absolute($dir))
+			eq fn_from_rel_fn($rel_directory)
+		) { 
+			next; 
+		}
 
-# push(@directory_list, { $url_filename => file_info(relative_file_name_from_absolute($dir)) });
         push( @directory_list,
             file_info( relative_file_name_from_absolute($dir) ) );
     }
 
     foreach my $file (@files) {
-        my $url_filename =
-          url_for_relative_filename( relative_file_name_from_absolute($file) );
+		
+		my $just_fn = fn_from_rel_fn(relative_file_name_from_absolute($file)); 
+		warn '$just_fn; ' . $just_fn if $t; 
+		# skip dot files/dirs
+		if($just_fn =~ m/^\./){ 
+			next; 
+		}
+		
+		
+		
+        my $url_filename = url_from_absolute_filename($file);
 
 # push(@directory_list, { $url_filename => file_info(relative_file_name_from_absolute($file)) });
         push( @directory_list,
@@ -235,11 +343,11 @@ sub rename {
         {
             "Error"    => $error ? "Could not rename" : "No error",
             "Code"     => $error,
-            "Old Path" => url_for_relative_filename(
+            "Old Path" => url_from_relative_filename(
                 relative_file_name_from_absolute($full_old)
             ),
             "Old Name" => $old_name,
-            "New Path" => url_for_relative_filename(
+            "New Path" => url_from_relative_filename(
                 relative_file_name_from_absolute($full_new)
             ),
             "New Name" => $new_name
@@ -279,15 +387,17 @@ sub add {
       if $t;
 
     my $path = $q->param('currentpath');
-    warn 'in add $path: ' . $path
+    warn '$path: ' . $path
       if $t;
 
     my $abs_path;
-    if ( $path =~ m/^\// ) {
-        $abs_path = $path;
+	# Nooooooo
+    if ( $path =~ m/^http/ ) {
+       $abs_path = absolute_file_name_from_url($path);
+	   warn 'after absolute_file_name_from_url $abs_path: ' . $abs_path if $t;
     }
     else {
-        $abs_path = absolute_file_name_from_url($path);
+        $abs_path = absolute_file_name_from_relative($path);
     }
 
     warn '$abs_path: ' . $abs_path
@@ -297,8 +407,15 @@ sub add {
 
     my $lightweight_fh = $q->upload('newfile');
     my $filename       = $q->param('newfile');
-    my $abs_filename   = $abs_path . "/" . $filename;
-    $filename = relative_file_name_from_absolute($abs_filename);
+    my $abs_filename   = r_ls($abs_path) . "/" . r_fs($filename);
+    
+	warn '$abs_path: ' . $abs_path if $t; 
+	warn '$abs_filename: ' . $abs_filename if $t;
+	
+	# right? 
+	$filename = r_ls($path) . "/" . r_fs($filename);
+	
+	# relative_file_name_from_absolute($abs_filename);
 
     my $buffer;
 
@@ -347,15 +464,30 @@ sub add {
 #Nice confusing path name for a folder!
 # ?mode=addfolder&path=/UserFiles/&name=new%20logo.png
 sub addfolder {
-    return unless params_valid( [qw(path name)] );
+    
+	warn 'in addfolder' if $t; 
+	
+	return unless params_valid( [qw(path name)] );
 
     my $path      = $q->param('path');
+	warn '$path: ' . $path if $t;
+	
     my $name      = $q->param('name');
-    my $full_path = absolute_file_name_from_url($path);
-    my $new_name  = relative_file_name_from_url($name)
-      ; #We don't really need to cast to absolute path, but this gives us '..' security for free
+	
+	warn '$name: ' . $name if $t;
+	
+	
+    my $full_path = absolute_file_name_from_relative($path);
+	
+	
+    my $new_name  = $name;
+	
+	if($new_name =~ m/\.\./){ 
+		die "no."
+	}
+	
 
-    my $success = mkdir $full_path . "/" . $new_name;
+    my $success = mkdir r_ls($full_path) . "/" . r_fs($new_name);
 
     print_json(
         {
@@ -392,7 +524,7 @@ sub download {
 }
 
 sub relative_file_name_from_url {
-    warn 'in relative_file_name_from_url'
+    warn "\nin relative_file_name_from_url\n"
       if $t;
 
     my $file = shift;
@@ -412,19 +544,97 @@ sub relative_file_name_from_url {
     my $qmeta_url_path = quotemeta( $config->{url_path} );
 
     $file =~ s/^$qmeta_url_path//;
+	$file =~ s/^\///; 
+	
+	my $fr = $config_js->{options}->{fileRoot};; 
+	   $fr =~ s/\/$//; 
+		
+	$file =  $fr . '/' . $file; 
+	
     warn '$file: ' . $file                                             if $t;
     warn 'remove_extra_slashes($file): ' . remove_extra_slashes($file) if $t;
     return remove_extra_slashes($file);
 }
 
+sub fn_from_rel_fn { 
+	
+	warn 'in fn_from_rel_fn' if $t;
+	my $fn = shift; 
+	   $fn =~ s/\/$|^\///g; 
+	
+	warn 'fn_from_rel_fn $fn:' . $fn if $t;
+	
+	my $fr = $config_js->{options}->{fileRoot};
+	   $fr =~ s/\/$|^\///g; 
+	   warn 'fn_from_rel_fn $fr: ' . $fr if $t;
+	   $fr = quotemeta($fr); 
+	   
+	   $fn =~ s/^$fr//; 
+	   $fn =~ s/\/$|^\///g; 
+	   
+	   warn 'finally fn_from_rel_fn $fn: ' . $fn if $t; 
+	   return $fn; 	   
+}
+
+sub rel_fn_from_fn { 
+	my $fn = shift; 
+	
+	my $rel_fn = $config_js->{options}->{fileRoot} . '/' . $fn; 
+	
+	return $rel_fn; 
+	
+}
+
 sub relative_file_name_from_absolute {
-    my $file = shift;
-    $file =~ s/$config->{uploads_directory}//;
+    
+	# Relative just means releatice to example: 
+	#/home/user/public_html/dada_mail_support_files/file_uploads
+	
+	warn "\nin relative_file_name_from_absolute\n" if $t;
+	
+	my $file = shift;
+	
+	warn '$file: ' . $file if $t; 
+	
+	warn '$config->{uploads_directory}: ' . $config->{uploads_directory} if $t; 
+	
+	my $abs_path = $config->{uploads_directory};
+	   $abs_path =~ s/\/$//; 
+    
+	$file =~ s/^$abs_path//;
+	$file =~ s/^\///;
+	
+	my $fr = $config_js->{options}->{fileRoot};
+	# take off last slash: 
+	$fr =~ s/\/$//; 
+	
+	warn '$fr: ' . $fr if $t;
+		
+	$file =  r_ls($fr) . '/' . r_fs($file); 
+	
+	warn '$file: ' . $file if $t;
+	
     return remove_extra_slashes($file);
 }
 
+
+sub r_ls { 
+	my $s = shift; 
+	   $s =~ s/\/$//;
+	   return $s; 
+}
+sub r_fs { 
+	my $s = shift; 
+	   $s =~ s/^\///;
+	   return $s; 
+}
+
+
+
+
+
 sub absolute_file_name_from_url {
-    warn 'in absolute_file_name_from_url' if $t;
+    warn "\nin absolute_file_name_from_url\n" if $t;
 
     my $file_path = shift;
 
@@ -434,35 +644,79 @@ sub absolute_file_name_from_url {
         error("Invalid file path");
         return undef;
     }
-    my $filename = $config->{uploads_directory} . '/'
-      . relative_file_name_from_url($file_path);
+	my $uploads_dir = $config->{uploads_directory}; 
+	
+	my $rel_fn      = relative_file_name_from_url($file_path); 
+	my $just_fn     = fn_from_rel_fn($rel_fn);
+	
+    my $filename =  r_ls($uploads_dir) . '/' . r_fs($just_fn);
+	  
+	warn 'final absolute_file_name_from_url $filename: ' . $filename 
+		if $t;  
+	
     return remove_extra_slashes($filename);
 }
 
 sub absolute_file_name_from_relative {
 
-    warn 'absolute_file_name_from_relative' if $t;
-
-    my $filename = $config->{uploads_directory} . "/" . shift;
-
-    warn 'remove_extra_slashes($filename): ' . remove_extra_slashes($filename)
+	my $rel_fn = shift; 
+   
+    warn "\nin absolute_file_name_from_relative\n" 
+		if $t;
+	
+	warn '$config->{uploads_directory}: ' . $config->{uploads_directory} if $t; 
+	warn '$rel_fn: ' . $rel_fn if $t;
+	
+	# $fileRoot is something like: /dada_mail_support_files/file_uploads/
+	my $fileRoot = $config_js->{options}->{fileRoot};
+	   $fileRoot = r_ls($fileRoot); 
+	   
+	warn '$fileRoot: ' . $fileRoot if $t;
+	$fileRoot = quotemeta($fileRoot);
+		
+	# $config->{uploads_directory} is something like, 
+	# /home/user/public_html/dada_mail_support_files/file_uploads/	
+	
+	# I just want the filename: 
+	$rel_fn =~ s/^$fileRoot//;	
+	
+	warn '$rel_fn: ' . $rel_fn if $t; 
+	
+	my $uploads_directory = $config->{uploads_directory} ; 
+	
+	# and then tack it on again: 
+	my $abs_fn = r_ls($uploads_directory) . '/' . r_fs($rel_fn);
+	
+	warn '$abs_fn: ' . $abs_fn if $t;
+	
+    warn 'remove_extra_slashes($abs_fn): ' . remove_extra_slashes($abs_fn)
       if $t;
-    return remove_extra_slashes($filename);
+	  
+    return remove_extra_slashes($abs_fn);
 }
 
-sub url_for_relative_filename {
-    warn 'in url_for_relative_filename'                if $t;
-    warn '$config->{url_path}: ' . $config->{url_path} if $t;
-
+sub url_from_relative_filename {
+    warn "\nin url_from_relative_filename\n"               
+		if $t;
+	
     my $filename = shift;
+	warn '$filename: ' . $filename if $t;
+	
+	$filename =~ s/^\///;
+	
+    warn '$config->{url_path}: ' . $config->{url_path} if $t;
+	
+	
+	$filename = fn_from_rel_fn($filename);
+	
+	warn '$filename: ' . $filename if $t; 
+	
+	my $url_path =  $config->{url_path}; 
+	   $url_path =~ s/\/$//;
+	   
+    my $url = $url_path . '/' . $filename;
 
-    warn '$filename: ' . $filename if $t;
-
-    $filename =~ s/^\///;
-
-    my $url = $config->{url_path} . '/' . $filename;
-
-    warn '$url: ' . $url if $t;
+    warn 'finally $url: ' . $url if $t;
 
     return $url;
 
@@ -470,6 +724,28 @@ sub url_for_relative_filename {
 
     #return remove_extra_slashes($url);
 }
+
+sub url_from_absolute_filename { 
+	
+	warn 'in url_from_absolute_filename' if $t; 
+	
+	
+	my $fn = shift; 
+	warn '$fn: ' . $fn if $t;  
+	
+	my $rfn = relative_file_name_from_absolute($fn);
+	warn '$rfn: ' . $rfn  if $t; 
+	
+	my $url =  url_from_relative_filename($rfn);
+
+	warn '$url: ' . $url if $t; 
+
+	test_url($url); 
+	
+	return $url; 
+}
+
+
 
 sub remove_extra_slashes {
     my $filename = shift;
